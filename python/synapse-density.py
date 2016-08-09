bf4b733fd44354076f406428a7c9eea9bb29565f
Steven Williams <stevencwilliams@gmail.com>
2016-08-09 06:00:50 -0400

    Add plot for synapse density

---
+++ python/synapse-density.py
@@ -0,0 +1,39 @@
+import argparse
+import plotlib
+import sys
+
+def parseArgs():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("runs", metavar = "RUNS", help = "runs directory")
+    parser.add_argument("--bin-width", metavar = "BIN_WIDTH", type = int, default = 1000, help = "bin width")
+    return parser.parse_args()
+
+args = parseArgs()
+figure = plotlib.getFigure()
+axes = figure.gca()
+runs = list(plotlib.getRuns(args.runs))
+fileName = "synapse-density.txt"
+for run in runs:
+    lifeSpans, births, deaths = plotlib.getLifeSpans(run)
+    values = plotlib.readAgentData(run, fileName)
+    if values is None:
+        values = {}
+        for agent in lifeSpans:
+            if agent % 1000 == 0:
+                sys.stderr.write("{0}\n".format(agent))
+            graph = plotlib.getGraph(run, agent, "incept")
+            neuronCount = len(graph)
+            synapseCount = 0
+            for preNeuron in range(neuronCount):
+                for postNeuron in range(neuronCount):
+                    if graph[preNeuron][postNeuron] is not None:
+                        synapseCount += 1
+            values[agent] = float(synapseCount) / (neuronCount * (neuronCount - 1))
+        plotlib.writeAgentData(run, fileName, values)
+    zipped = plotlib.zipAgentData(births, values)
+    binned = plotlib.binData(zipped[0], zipped[1], args.bin_width)
+    axes.plot(binned[0], binned[1], alpha = 1.0 / len(runs))
+axes.set_xlabel("Timestep")
+axes.set_ylabel("Synapse density")
+figure.tight_layout()
+figure.savefig("synapse-density.pdf")
