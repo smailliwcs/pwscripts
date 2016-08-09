7c802a3197863cc25c3b8170e1e5cf7529e120e7
Steven Williams <stevencwilliams@gmail.com>
2016-08-09 05:47:23 -0400

    Add plot for neuron count

---
+++ python/neuron-count.py
@@ -0,0 +1,33 @@
+import argparse
+import plotlib
+import sys
+
+def parseArgs():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("runs", metavar = "RUNS", help = "runs directory")
+    parser.add_argument("--bin-width", metavar = "BIN_WIDTH", type = int, default = 1000, help = "bin width")
+    return parser.parse_args()
+
+args = parseArgs()
+figure = plotlib.getFigure()
+axes = figure.gca()
+runs = list(plotlib.getRuns(args.runs))
+fileName = "neuron-count.txt"
+for run in runs:
+    lifeSpans, births, deaths = plotlib.getLifeSpans(run)
+    values = plotlib.readAgentData(run, fileName)
+    if values is None:
+        values = {}
+        for agent in lifeSpans:
+            if agent % 1000 == 0:
+                sys.stderr.write("{0}\n".format(agent))
+            graph = plotlib.getGraph(run, agent, "incept")
+            values[agent] = len(graph)
+        plotlib.writeAgentData(run, fileName, values)
+    zipped = plotlib.zipAgentData(births, values)
+    binned = plotlib.binData(zipped[0], zipped[1], args.bin_width)
+    axes.plot(binned[0], binned[1], alpha = 1.0 / len(runs))
+axes.set_xlabel("Timestep")
+axes.set_ylabel("Neuron count")
+figure.tight_layout()
+figure.savefig("neuron-count.pdf")
