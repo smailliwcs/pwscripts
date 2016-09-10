7440d4239927494e16ae866f9e0154394fc2e2bd
Steven Williams <stevencwilliams@gmail.com>
2016-09-10 09:34:52 -0400

    Remove combined synapse weight plot

--- python/synapse-weight.py
+++ python/synapse-weight.py
@@ -5,12 +5,17 @@ import sys
 def parseArgs():
     parser = argparse.ArgumentParser()
     parser.add_argument("runs", metavar = "RUNS", help = "runs directory")
-    parser.add_argument("type", metavar = "TYPE", choices = ("pos", "neg", "both", "abs"), help = "weight type")
+    parser.add_argument("type", metavar = "TYPE", choices = ("excitatory", "inhibitory", "absolute"), help = "weight type")
     parser.add_argument("--bin-width", metavar = "BIN_WIDTH", type = int, default = 1000, help = "bin width")
     return parser.parse_args()
 
-def getValues(type, births):
-    fileName = "synapse-weight-{0}.txt".format(type)
+args = parseArgs()
+figure = plotlib.getFigure()
+axes = figure.gca()
+runs = list(plotlib.getRuns(args.runs))
+fileName = "synapse-weight-{0}.txt".format(args.type)
+for run in runs:
+    births = plotlib.getBirths(run)
     values = plotlib.readAgentData(run, fileName)
     if values is None:
         values = {}
@@ -24,45 +29,18 @@ def getValues(type, births):
                     weight = graph.weights[preNeuron][postNeuron]
                     if weight is None:
                         continue
-                    if type == "pos" and weight >= 0:
+                    if args.type == "excitatory" and weight >= 0:
                         weights.append(weight)
-                    elif type == "neg" and weight <= 0:
+                    elif args.type == "inhibitory" and weight <= 0:
                         weights.append(-weight)
-                    elif type == "abs":
+                    elif args.type == "absolute":
                         weights.append(abs(weight))
             values[agent] = plotlib.getMean(weights)
         plotlib.writeAgentData(run, fileName, values)
-    return values
-
-def getLabel(type):
-    metric = "Synapse weight"
-    if type == "pos":
-        return "{0} (excitatory)".format(metric)
-    elif type == "neg":
-        return "{0} (inhibitory)".format(metric)
-    else:
-        return metric
-
-args = parseArgs()
-figure = plotlib.getFigure()
-axes = figure.gca()
-runs = list(plotlib.getRuns(args.runs))
-if args.type == "both":
-    types = ("pos", "neg")
-else:
-    types = (args.type,)
-for run in runs:
-    births = plotlib.getBirths(run)
-    for type in types:
-        values = getValues(type, births)
-        zipped = plotlib.zipAgentData(births, values)
-        binned = plotlib.binData(zipped[0], zipped[1], args.bin_width)
-        if args.type == "both" and type == "neg":
-            dashes = plotlib.dashes
-        else:
-            dashes = ()
-        axes.plot(binned[0], binned[1], dashes = dashes, alpha = 1.0 / len(runs))
+    zipped = plotlib.zipAgentData(births, values)
+    binned = plotlib.binData(zipped[0], zipped[1], args.bin_width)
+    axes.plot(binned[0], binned[1], alpha = 1.0 / len(runs))
 axes.set_xlabel("Timestep")
-axes.set_ylabel(getLabel(args.type))
+axes.set_ylabel("Synapse weight ({0})".format(args.type))
 figure.tight_layout()
 figure.savefig("synapse-weight-{0}.pdf".format(args.type))
