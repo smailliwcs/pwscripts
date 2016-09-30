193f506b694a1170c434589ff11781217e83d48f
Steven Williams <stevencwilliams@gmail.com>
2016-09-30 08:12:41 -0400

    Use univariate entropy calculator

--- java/Entropy.java
+++ java/Entropy.java
@@ -1,12 +1,13 @@
 import infodynamics.measures.continuous.*;
-import infodynamics.measures.continuous.kozachenko.*;
+import infodynamics.measures.continuous.kernel.*;
+import infodynamics.utils.*;
 import java.util.*;
 
 public class Entropy {
-    private static EntropyCalculatorMultiVariate calculator;
+    private static EntropyCalculator calculator;
     
     public static void main(String[] args) throws Exception {
-        calculator = new EntropyCalculatorMultiVariateKozachenko();
+        calculator = new EntropyCalculatorKernel();
         Utility.setProperties(calculator, System.out);
         try (TimeSeriesEnsembleReader reader = new TimeSeriesEnsembleReader(System.in)) {
             reader.printArguments(System.out);
@@ -22,8 +23,13 @@ public class Entropy {
     }
     
     private static double calculate(TimeSeriesEnsemble ensemble, int[] neuronIndices) throws Exception {
-        calculator.initialise(neuronIndices.length);
-        calculator.setObservations(ensemble.getCombinedTimeSeries().get(neuronIndices));
-        return calculator.computeAverageLocalOfObservations();
+        TimeSeries timeSeries = ensemble.getCombinedTimeSeries();
+        double[] values = new double[neuronIndices.length];
+        for (int valueIndex = 0; valueIndex < neuronIndices.length; valueIndex++) {
+            calculator.initialise();
+            calculator.setObservations(timeSeries.get(neuronIndices[valueIndex]));
+            values[valueIndex] = calculator.computeAverageLocalOfObservations();
+        }
+        return MatrixUtils.mean(values);
     }
 }
