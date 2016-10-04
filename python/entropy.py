0dd85ba3f2859aa0a6178420ddee20cf21afdfcf
Steven Williams <stevencwilliams@gmail.com>
2016-10-04 05:05:16 -0400

    Add plot for entropy

---
+++ python/entropy.py
@@ -0,0 +1,35 @@
+import argparse
+import os
+import plotlib
+
+def parseArgs():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("runs", metavar = "RUNS", help = "runs directory")
+    parser.add_argument("stage", metavar = "STAGE", choices = ("incept", "birth", "death"), help = "life stage")
+    parser.add_argument("stat", metavar = "STAT", choices = ("mean", "sum"), help = "statistic")
+    parser.add_argument("--bin-width", metavar = "BIN_WIDTH", type = int, default = 1000, help = "bin width")
+    return parser.parse_args()
+
+args = parseArgs()
+figure = plotlib.getFigure()
+axes = figure.gca()
+runs = list(plotlib.getRuns(args.runs))
+for run in runs:
+    births = plotlib.getBirths(run)
+    values = {}
+    path = os.path.join(run, "plots", "data", "entropy-{0}.txt".format(args.stage))
+    with open(path) as f:
+        for line in f:
+            if line.startswith("#"):
+                continue
+            agent, neuron, value = line.split()
+            values.setdefault(int(agent), []).append(float(value))
+    for agent in values:
+        values[agent] = plotlib.getStatistic(values[agent], args.stat)
+    zipped = plotlib.zipAgentData(births, values)
+    binned = plotlib.binData(zipped[0], zipped[1], args.bin_width)
+    axes.plot(binned[0], binned[1], alpha = 1.0 / len(runs))
+axes.set_xlabel("Timestep")
+axes.set_ylabel("Entropy")
+figure.tight_layout()
+figure.savefig("entropy-{0}-{1}.pdf".format(args.stage, args.stat))
