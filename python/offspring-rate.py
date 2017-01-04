f92cc0ef718379943c9bc856d15ee52c0dc68e33
Steven Williams <stevencwilliams@gmail.com>
2017-01-04 05:11:59 -0500

    Add plot for offspring rate

---
+++ python/offspring-rate.py
@@ -0,0 +1,27 @@
+import argparse
+import plotlib
+
+def parseArgs():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("runs", metavar = "RUNS", help = "runs directory")
+    parser.add_argument("--bin-width", metavar = "BIN_WIDTH", type = int, default = 1000, help = "bin width")
+    return parser.parse_args()
+
+args = parseArgs()
+figure = plotlib.getFigure()
+axes = figure.gca()
+runs = list(plotlib.getRuns(args.runs))
+for run in runs:
+    births = plotlib.getBirths(run)
+    lifeSpans = plotlib.getLifeSpans(run)
+    offspring = plotlib.getOffspring(run)
+    values = {}
+    for agent in offspring:
+        values[agent] = float(len(offspring[agent])) / lifeSpans[agent]
+    zipped = plotlib.zipAgentData(births, values)
+    binned = plotlib.binData(zipped[0], zipped[1], args.bin_width)
+    axes.plot(binned[0], binned[1], alpha = 1.0 / len(runs))
+axes.set_xlabel("Timestep")
+axes.set_ylabel("Offspring rate")
+figure.tight_layout()
+figure.savefig("offspring-rate.pdf")
