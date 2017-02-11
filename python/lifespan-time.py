259d5f76518b92e45bea90bb23423534b9e31f64
Steven Williams <stevencwilliams@gmail.com>
2017-02-11 07:37:48 -0500

    Make "life span" one word

--- python/lifespan-time.py
+++ python/lifespan-time.py
@@ -18,6 +18,6 @@ for run in runs:
     binned = plotlib.binData(zipped[0], zipped[1], args.bin_width)
     axes.plot(binned[0], binned[1], alpha = 1.0 / len(runs))
 axes.set_xlabel("Timestep")
-axes.set_ylabel("Life span")
+axes.set_ylabel("Lifespan")
 figure.tight_layout()
-figure.savefig("life-span-time.pdf")
+figure.savefig("lifespan-time.pdf")
