74f85ffb7206c6755e738067fcddeed58c594a70
Steven Williams <stevencwilliams@gmail.com>
2017-02-13 08:21:37 -0500

    Consolidate information dynamics plotting code

--- python/info-dynamics-custom.py
+++ python/info-dynamics-custom.py
@@ -6,7 +6,7 @@ def parseArgs():
     parser = argparse.ArgumentParser()
     parser.add_argument("runs", metavar = "RUNS", help = "runs directory")
     parser.add_argument("stage", metavar = "STAGE", choices = ("incept", "birth", "death"), help = "life stage")
-    parser.add_argument("stat", metavar = "STAT", choices = ("mean", "median", "sum"), help = "statistic")
+    parser.add_argument("metric", metavar = "METRIC", choices = ("storage", "transfer", "modification"), help = "metric type")
     parser.add_argument("--bin-width", metavar = "BIN_WIDTH", type = int, default = 1000, help = "bin width")
     return parser.parse_args()
 
@@ -17,20 +17,18 @@ runs = list(plotlib.getRuns(args.runs))
 for run in runs:
     births = plotlib.getBirths(run)
     values = {}
-    path = os.path.join(run, "plots", "data", "info-transfer-jidt-apparent-{0}.txt".format(args.stage))
+    path = os.path.join(run, "plots", "data", "info-{0}-custom-{1}.txt".format(args.metric, args.stage))
     with open(path) as f:
         for line in f:
             if line.startswith("#"):
                 continue
-            agent, preNeuron, postNeuron, value = line.split()
-            values.setdefault(int(agent), []).append(float(value))
-    for agent in values:
-        values[agent] = plotlib.getStatistic(values[agent], args.stat)
+            agent, value = line.split()
+            values[int(agent)] = float(value)
     zipped = plotlib.zipAgentData(births, values)
     binned = plotlib.binData(zipped[0], zipped[1], args.bin_width)
     axes.plot(binned[0], binned[1], alpha = 1.0 / len(runs))
 axes.set_xlabel("Timestep")
-axes.set_ylabel("Apparent transfer entropy")
+axes.set_ylabel("Information {0}".format(args.metric))
 axes.set_ylim(bottom = max(0, axes.get_ylim()[0]))
 figure.tight_layout()
-figure.savefig("info-transfer-jidt-apparent-{0}-{1}.pdf".format(args.stage, args.stat))
+figure.savefig("info-{0}-custom-{1}.pdf".format(args.metric, args.stage))
