33a6528c0bd0f37ab9ee80639b303e7fcce0aa26
Steven Williams <stevencwilliams@gmail.com>
2017-12-17 09:12:47 -0500

    Automatically refresh online monitor

--- python/monitor.py
+++ python/monitor.py
@@ -1,51 +1,67 @@
 from pylab import *
 import argparse
 import os
+import sys
 import utility
 
 parser = argparse.ArgumentParser()
 parser.add_argument("run", metavar = "RUN")
 args = parser.parse_args()
+ion()
 
-subplot(3, 1, 1)
-x = []
-y = []
-with open(os.path.join(args.run, "population.txt")) as f:
-    for line in f:
-        if line == "\n" or line.startswith("#"):
-            continue
-        chunks = line.split()
-        x.append(int(chunks[0]))
-        y.append(int(chunks[1]))
-plot(x, y)
+def population():
+    x = []
+    y = []
+    with open(os.path.join(args.run, "population.txt")) as f:
+        for line in f:
+            if line == "\n" or line.startswith("#"):
+                continue
+            chunks = line.split()
+            x.append(int(chunks[0]))
+            y.append(int(chunks[1]))
+    subplot(3, 1, 1)
+    plot(x, y)
 
-subplot(3, 1, 2)
-x = []
-ys = []
-with open(os.path.join(args.run, "energy/food.txt")) as f:
-    for line in f:
-        if line.startswith("#@L"):
-            for index in xrange(len(line.split()) - 2):
-                ys.append([])
-            continue
-        elif line == "\n" or line.startswith("#"):
-            continue
-        chunks = line.split()
-        x.append(int(chunks[0]))
-        for index in xrange(len(ys)):
-            ys[index].append(float(chunks[1 + index]))
-for index in xrange(len(ys)):
-    plot(x, ys[index])
+def food():
+    x = []
+    ys = []
+    cs = ["g", "r"]
+    with open(os.path.join(args.run, "energy/food.txt")) as f:
+        for line in f:
+            if line.startswith("#@L"):
+                for index in xrange(len(line.split()) - 2):
+                    ys.append([])
+                continue
+            elif line == "\n" or line.startswith("#"):
+                continue
+            chunks = line.split()
+            x.append(int(chunks[0]))
+            for index in xrange(len(ys)):
+                ys[index].append(float(chunks[1 + index]))
+    subplot(3, 1, 2)
+    for index in xrange(len(ys)):
+        plot(x, ys[index], cs[index % len(cs)])
 
-subplot(3, 1, 3)
-y = []
-with open(os.path.join(args.run, "lifespans.txt")) as f:
-    for line in f:
-        if line == "\n" or line.startswith("#"):
-            continue
-        chunks = line.split()
-        y.append(int(chunks[3]) - int(chunks[1]))
-hist(y, bins = 50, range = [0, 1000])
-xticks(range(0, 1001, 100))
+def lifespans():
+    y = []
+    with open(os.path.join(args.run, "lifespans.txt")) as f:
+        for line in f:
+            if line == "\n" or line.startswith("#"):
+                continue
+            chunks = line.split()
+            y.append(int(chunks[3]) - int(chunks[1]))
+    subplot(3, 1, 3)
+    hist(y, bins = 20, range = [0, 500])
 
-show()
+def _plot(fn):
+    try:
+        fn()
+    except Exception as ex:
+        sys.stderr.write("{0}\n".format(ex))
+
+while True:
+    _plot(population)
+    _plot(food)
+    _plot(lifespans)
+    pause(10)
+    clf()
