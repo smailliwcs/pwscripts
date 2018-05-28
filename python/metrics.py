b359096ad0575f7133488a435314fd4e57415806
Steven Williams <stevencwilliams@gmail.com>
2018-05-28 09:53:04 -0400

    Aggregate entropy by mean

--- python/metrics.py
+++ python/metrics.py
@@ -517,9 +517,6 @@ class Entropy(AgentBasedMetric):
     
     def getLabel(self):
         return "Differential entropy"
-    
-    def aggregate(self, values):
-        return numpy.nanmedian(values)
 
 class Expansion(AgentBasedMetric):
     def addArgs(self, parser):
