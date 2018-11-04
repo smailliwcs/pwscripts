81b8e2015e12c1a7f846b49894b8b9c673eb40bc
Steven Williams <stevencwilliams@gmail.com>
2018-11-04 13:29:27 -0500

    Normalize adaptivity at 99.9th percentile

--- python/fitness.py
+++ python/fitness.py
@@ -1,6 +1,7 @@
 import argparse
 import math
 import metrics as metrics_mod
+import numpy
 import operator
 import os
 import sys
@@ -14,36 +15,27 @@ def parseArgs():
 
 args = parseArgs()
 data = {}
-quantiles = {}
+caps = {}
 metric = metrics_mod.Adaptivity()
 metric.condition = args.condition
 types = list(metrics_mod.Adaptivity.Type.getValues())
 for type in types:
     sys.stderr.write("{0}\n".format(type))
-    data[type] = []
-    quantiles[type] = {}
+    data[type] = {}
+    values = []
     metric.type = type
     for run in utility.getRuns(args.runs):
         sys.stderr.write("{0}\n".format(run))
-        quantiles[type][run] = {}
+        data[type][run] = {}
         metric.initialize(run)
         for agent, value in metric.read().iteritems():
-            data[type].append({
+            data[type][run][agent] = {
                 "run": run,
                 "agent": agent,
                 "value": value
-            })
-    data[type].sort(key = operator.itemgetter("value"), reverse = True)
-    count = len(data[type])
-    lastValue = None
-    sameCount = 0
-    for index, datum in enumerate(data[type]):
-        if datum["value"] == lastValue:
-            sameCount += 1
-        else:
-            sameCount = 0
-        quantiles[type][datum["run"]][datum["agent"]] = 1.0 - float(index - sameCount) / (count - 1)
-        lastValue = datum["value"]
+            }
+            values.append(value)
+    caps[type] = numpy.nanpercentile(values, 99.9)
 if args.condition is None:
     name = "fitness.txt"
 else:
@@ -52,7 +44,11 @@ for run in utility.getRuns(args.runs):
     path = os.path.join(run, "data", name)
     with open(path, "w") as f:
         for agent in utility.getAgents(run):
-            values = [quantiles[type][run][agent] for type in types]
-            assert len(values) == 3
-            fitness = math.sqrt(sum(map(lambda value: value ** 2, values)) / len(values))
+            values = []
+            for type in types:
+                value = data[type][run][agent]["value"] / caps[type]
+                if value > 1.0:
+                    value = 1.0
+                values.append(value)
+            fitness = numpy.nanmean(values)
             f.write("{0} {1}\n".format(agent, fitness))
